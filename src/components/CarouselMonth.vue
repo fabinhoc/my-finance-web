de7100e5ccb8dec6529018794794ac94c8a71c80
Fabio Cruz <fabinhocosta1202@gmail.com>
2022-12-01 21:40:53 -0300

    :hammer: Add month and year component

---
+++ src/components/CarouselMonth.vue
@@ -0,0 +1,92 @@
+<template>
+  <q-carousel
+    v-model="slide"
+    transition-prev="slide-right"
+    transition-next="slide-left"
+    animated
+    control-color="dark"
+    class="rounded-borders"
+    padding
+    arrows
+    infinite
+    height="90px"
+  >
+    <q-carousel-slide
+      v-for="month in months"
+      :key="month.monthInNumber"
+      :name="month.monthInNumber"
+      class="column no-wrap flex-center"
+    >
+      <div class="text-center text-h6 text-primary text-weight-light">
+        {{ month.stringMonth }}
+      </div>
+    </q-carousel-slide>
+  </q-carousel>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, Ref } from 'vue';
+import { MonthType } from 'src/types/Month.type';
+
+export default defineComponent({
+  name: 'CarouselMonth',
+  setup() {
+    const slide = ref('11');
+    const months: Ref<MonthType[]> = ref([
+      {
+        monthInNumber: '01',
+        stringMonth: 'Janeiro',
+      },
+      {
+        monthInNumber: '02',
+        stringMonth: 'Fevereiro',
+      },
+      {
+        monthInNumber: '03',
+        stringMonth: 'Março',
+      },
+      {
+        monthInNumber: '04',
+        stringMonth: 'Abril',
+      },
+      {
+        monthInNumber: '05',
+        stringMonth: 'Maio',
+      },
+      {
+        monthInNumber: '06',
+        stringMonth: 'Junho',
+      },
+      {
+        monthInNumber: '07',
+        stringMonth: 'Julho',
+      },
+      {
+        monthInNumber: '08',
+        stringMonth: 'Agosto',
+      },
+      {
+        monthInNumber: '09',
+        stringMonth: 'Setembro',
+      },
+      {
+        monthInNumber: '10',
+        stringMonth: 'Outubro',
+      },
+      {
+        monthInNumber: '11',
+        stringMonth: 'Novembro',
+      },
+      {
+        monthInNumber: '12',
+        stringMonth: 'Dezembro',
+      },
+    ]);
+
+    return {
+      slide,
+      months,
+    };
+  },
+});
+</script>
---
+++ src/components/CarouselYear.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-carousel
+    v-model="slide"
+    transition-prev="slide-right"
+    transition-next="slide-left"
+    animated
+    control-color="dark"
+    class="rounded-borders"
+    arrows
+    infinite
+    height="58px"
+  >
+    <q-carousel-slide
+      v-for="slide in slides"
+      :key="slide"
+      :name="slide"
+      class="column no-wrap flex-center"
+    >
+      <div class="text-center text-h3 text-primary text-weight-regular">
+        {{ slide }}
+      </div>
+    </q-carousel-slide>
+  </q-carousel>
+</template>
+
+<script lang="ts">
+import { watch, defineComponent, Ref, ref } from 'vue';
+
+export default defineComponent({
+  name: 'CarouselYear',
+  setup() {
+    const slide = ref(2022);
+    const year = new Date().getFullYear();
+    let slides: Ref<number[]> = ref([year - 1, year, year + 1]);
+    watch(slide, (newValue) => {
+      slides.value = [newValue - 1, newValue, newValue + 1];
+    });
+
+    return {
+      slide,
+      slides,
+    };
+  },
+});
+</script>
